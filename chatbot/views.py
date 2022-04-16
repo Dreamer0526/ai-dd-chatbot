569d9ceec9fc52e4d2da06879bef97743bf59dec
Mengzhu Xie <xiemengzhu0526@gmail.com>
2022-04-16 23:06:39 +0800

    check signature

--- chatbot/views.py
+++ chatbot/views.py
@@ -4,21 +4,22 @@ import hashlib
 from django.http import HttpResponse
 
 
-def check_token(request):
+def check_signature(request):
     phrase = "186548"
     nonce = request.GET["nonce"]
     echostr = request.GET["echostr"]
     timestamp = request.GET["timestamp"]
+    signature = request.GET["signature"]
 
     auth_list = [phrase, timestamp, nonce]
     auth_list.sort()
     auth_str = "".join(auth_list).encode("utf-8")
-    hash_result = hashlib.sha1(auth_str).hexdigest()
+    hashcode = hashlib.sha1(auth_str).hexdigest()
 
-    if hash_result == echostr:
+    if signature == hashcode:
         return HttpResponse(echostr)
     else:
-        return HttpResponse("error")
+        return HttpResponse("token error")
 
 
 def gen_response(to_user_open_id, from_user_open_id, content):
@@ -42,7 +43,7 @@ def gen_response(to_user_open_id, from_user_open_id, content):
 
 def index(request):
     if request.method == "GET":
-        return check_token(request)
+        return check_signature(request)
 
     if request.method == "POST":
         response = gen_response(request.POST["openid"], "gh_74ccc0ad896d", "content")
