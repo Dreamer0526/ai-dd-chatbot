10bd55e14d57dd79d985d637f19eb48e1530a930
Mengzhu Xie <xiemengzhu0526@gmail.com>
2022-04-16 22:37:13 +0800

    feat: return msg in xml

--- chatbots/views.py
+++ chatbots/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.http import HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
 
@@ -7,4 +9,21 @@ def index(request):
         echostr = request.GET["echostr"]
         return HttpResponse(echostr)
     except MultiValueDictKeyError:
-        return HttpResponse("Hello, world. You're at the polls index.")
+        response_dict = dict()
+        response_dict["ToUserName"] = request.POST["openid"]
+        response_dict["FromUserName"] = "gh_74ccc0ad896d"
+        response_dict["CreateTime"] = int(time.time())
+        response_dict["Content"] = "content"
+
+        XmlForm = """
+                <xml>
+                    <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+                    <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+                    <CreateTime>{CreateTime}</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[{Content}]]></Content>
+                </xml>
+                """
+        response = XmlForm.format(**response_dict)
+
+        return HttpResponse(response, content_type="application/xml")
